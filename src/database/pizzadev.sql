bac59b365ca9de15dc0bf03db70ec90df37d845f
Filipe <filipess671@gmail.com>
2023-06-20 12:03:06 -0300

    Feat: Código do SQL

---
+++ src/database/pizzadev.sql
@@ -0,0 +1,71 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.32 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.4.0.6659
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para pizzadev
+DROP DATABASE IF EXISTS `pizzadev`;
+CREATE DATABASE IF NOT EXISTS `pizzadev` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pizzadev`;
+
+-- Copiando estrutura para tabela pizzadev.mensagem
+DROP TABLE IF EXISTS `mensagem`;
+CREATE TABLE IF NOT EXISTS `mensagem` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `assunto` varchar(100) NOT NULL,
+  `mensagem` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela pizzadev.mensagem: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela pizzadev.pizza
+DROP TABLE IF EXISTS `pizza`;
+CREATE TABLE IF NOT EXISTS `pizza` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) DEFAULT NULL,
+  `descricao` text,
+  `precoBrotinho` double DEFAULT NULL,
+  `precoMedia` double DEFAULT NULL,
+  `precoGrande` double DEFAULT NULL,
+  `foto` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela pizzadev.pizza: ~1 rows (aproximadamente)
+INSERT INTO `pizza` (`id`, `nome`, `descricao`, `precoBrotinho`, `precoMedia`, `precoGrande`, `foto`) VALUES
+	(2, 'Pizza de marguerita', '', 25.8, 35, 45, 'pizza-marguerita.jpg');
+
+-- Copiando estrutura para tabela pizzadev.usuario
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `nome` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `usuario` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `senha` varbinary(128) NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- Copiando dados para a tabela pizzadev.usuario: ~1 rows (aproximadamente)
+INSERT INTO `usuario` (`nome`, `usuario`, `senha`, `token`) VALUES
+	('admin', 'admin', _binary 0x22797d8a1b4ae09a9d45a11fa512d9b8, NULL);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
